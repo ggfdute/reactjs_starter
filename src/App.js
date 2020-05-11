bf46fa2af63d8fb48fdcd4451192a3a3fc9c4fe1
ggfdute <david.geho@aol.com>
2020-05-11 19:24:52 +0000

    Forgot password page in and sent page in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
